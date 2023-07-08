31cbd260807a0ab651055142a66e89a64eca0323
Igor Buosi <igorbiancobuosi@hotmail.com>
2023-07-08 19:27:45 -0300

    html e css para tecnico list

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnicos';
 
 @Component({
   selector: 'app-tecnico-list',
   templateUrl: './tecnico-list.component.html',
   styleUrls: ['./tecnico-list.component.css']
 })
-export class TecnicoListComponent {
+export class TecnicoListComponent implements OnInit {
+
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: 'Igor', 
+      cpf: '456.472.428-20',
+      email: 'igorbiancobuosi@hotmail.com',
+      senha: '1234',
+      perfis: ['0'],
+      dataCriacao: '01/01/2000'
+    }
+  ] 
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
+  constructor(){}
+
+  ngOnInit():void{
+
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
 }
+
+
